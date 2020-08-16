408dc43cc2aecf681410257f5a0444d678c10071
Chris Reuenthal <creuenthal@paxata.com>
2020-08-15 20:13:48 -0400

    Initial Spring Boot Setup

---
+++ src/test/java/io/reuenthal/rhove/RhoveApplicationTests.java
@@ -0,0 +1,13 @@
+package io.reuenthal.rhove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
