8d93b508efa213455f2da602a4301a36d5621441
Chris Reuenthal <vehementus@gmail.com>
2020-08-16 23:32:29 -0400

    Adding React UI, plugged into server

---
+++ src/main/resources/precache-manifest.896bced5764f364fcc7ddfc67a4d0b77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acc913c8d58a2d549720a9eb5f105341",
+    "url": "/index.html"
+  },
+  {
+    "revision": "913fc931f4763325f43a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0accc51b0801c454c1c7",
+    "url": "/static/js/2.0b607357.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b607357.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "913fc931f4763325f43a",
+    "url": "/static/js/main.77e707aa.chunk.js"
+  },
+  {
+    "revision": "e30d0e563c23507ca164",
+    "url": "/static/js/runtime-main.00096a9e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
